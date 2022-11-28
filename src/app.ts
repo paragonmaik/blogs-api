fa7b6e19f02867162452c7896a00adee0dddc698
Maikson <estevan.maik@gmail.com>
2022-11-28 08:11:31 -0300

    chore: adiciona cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import routers from "./routers";
 import morgan from "morgan";
+import cors from "cors";
 import { ErrorHandler } from "./middlewares/errorHandler";
 
 const app = express();
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(routers);
