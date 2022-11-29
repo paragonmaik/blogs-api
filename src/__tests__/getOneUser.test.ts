c3a43f264382636336a1e84551fb0b5005df2782
Maikson <estevan.maik@gmail.com>
2022-11-29 16:03:35 -0300

    fix: ajusta rota de testes

--- src/__tests__/getOneUser.test.ts
+++ src/__tests__/getOneUser.test.ts
@@ -3,13 +3,15 @@ import app from "../app";
 import { StatusCodes } from "http-status-codes";
 
 describe("GET /users/:id", () => {
+	describe("User with valid token", () => {});
+
 	describe("With invalid authorization field", () => {
 		const invalidTokenCases = ["invalid-token", ""];
 
 		it("should respond with status code 401", async () => {
 			for (const token of invalidTokenCases) {
 				const response = await request(app)
-					.get("/user")
+					.get("/user/1")
 					.set("Authorization", token);
 
 				expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
@@ -18,7 +20,7 @@ describe("GET /users/:id", () => {
 
 		it("should respond with correct message in case of empty token", async () => {
 			const response = await request(app)
-				.get("/user")
+				.get("/user/1")
 				.set("Authorization", invalidTokenCases[1]);
 
 			expect(response.body.message).toBe("Token not found");
@@ -26,7 +28,7 @@ describe("GET /users/:id", () => {
 
 		it("should respond with correct message in case of invalid token", async () => {
 			const response = await request(app)
-				.get("/user")
+				.get("/user/1")
 				.set("Authorization", invalidTokenCases[0]);
 
 			expect(response.body.message).toBe("Expired or invalid token");
