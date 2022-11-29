81a6ff24f7425e9f9f433c4781af36d2df516a3e
Maikson <estevan.maik@gmail.com>
2022-11-29 15:08:56 -0300

    refactor: refatora controller para usar o middleware

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,26 +2,11 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { registerUser } from "../services/user.service";
 import { IUser } from "../interfaces/IUser";
-import { authToken } from "../utils/auth";
 import { getUsersList } from "../services/user.service";
 
-export const getAllUsers = async (req: Request, res: Response) => {
-	const token = req.headers.authorization;
-	if (!token) {
-		return res
-			.status(StatusCodes.UNAUTHORIZED)
-			.json({ message: "Token not found" });
-	}
-	const payload = await authToken(token);
-	if (typeof payload === "string") {
-		return { message: "teste" };
-	}
-	res.locals.payload = payload;
-
-	if (payload.status === 401) {
-		return res.status(payload.status).json({ message: payload.message });
-	}
+export const getAllUsers = async (_req: Request, res: Response) => {
 	const response = await getUsersList();
+
 	res.status(StatusCodes.OK).json(response);
 };
 
