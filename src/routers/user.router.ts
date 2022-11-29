8e738d74254917a50cce03c032c2238638789faf
Maikson <estevan.maik@gmail.com>
2022-11-29 14:08:12 -0300

    feat: implementa rota GET /users

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller";
+import { createUser, getAllUsers } from "../controllers/user.controller";
 import { UserSchema } from "../middlewares/joiSchemas/user.schema";
 import { validateSchema } from "../middlewares/validateSchemas";
 require("express-async-errors");
 
 const routers = express.Router();
 
+routers.get("/", getAllUsers);
 routers.post("/", validateSchema(UserSchema), createUser);
 
 export default routers;
