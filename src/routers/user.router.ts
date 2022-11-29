de55b94eb34355abcf90e7fa9db9071ffe1ebe1e
Maikson <estevan.maik@gmail.com>
2022-11-29 15:08:28 -0300

    chore: adiciona middleware à rota GET /users

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,11 +2,12 @@ import express from "express";
 import { createUser, getAllUsers } from "../controllers/user.controller";
 import { UserSchema } from "../middlewares/joiSchemas/user.schema";
 import { validateSchema } from "../middlewares/validateSchemas";
+import { authenticationMiddleware } from "../middlewares/auth.middleware";
 require("express-async-errors");
 
 const routers = express.Router();
 
-routers.get("/", getAllUsers);
+routers.get("/", authenticationMiddleware, getAllUsers);
 routers.post("/", validateSchema(UserSchema), createUser);
 
 export default routers;
