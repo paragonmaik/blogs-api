1cd739b92666882bdc0f7fb99c2c999e9ce01987
Maikson <estevan.maik@gmail.com>
2022-11-30 13:26:41 -0300

    chore: configura arquivos de rotas

---
+++ src/routers/category.router.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { authenticationMiddleware } from "../middlewares/auth.middleware";
+require("express-async-errors");
+
+const routers = express.Router();
+routers.post("/");
+
+export default routers;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import loginRouter from "./login.router";
 import userRouter from "./user.router";
+import categoryRouter from "./category.router";
 
 const routers = express.Router();
 
 routers.use("/login", loginRouter);
 routers.use("/user", userRouter);
+routers.use("/categories", categoryRouter);
 
 export default routers;
