592f01c9ceae77bad8200485663270843edebfd5
Maikson <estevan.maik@gmail.com>
2022-12-01 09:56:15 -0300

    chore: adiciona função createNewPost à rota POST /post

--- src/routers/post.router.ts
+++ src/routers/post.router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authenticationMiddleware } from "../middlewares/auth.middleware";
+import { createNewPost } from "../controllers/post.controller";
 require("express-async-errors");
 
 const routers = express.Router();
 
-routers.post("/", authenticationMiddleware);
+routers.post("/", authenticationMiddleware, createNewPost);
 
 export default routers;
