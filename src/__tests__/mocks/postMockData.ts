fae5ee1dcfdd81dcdd6059624678c3db83131930
Maikson <estevan.maik@gmail.com>
2022-12-01 14:47:07 -0300

    Empty-Commit

--- src/__tests__/mocks/postMockData.ts
+++ src/__tests__/mocks/postMockData.ts
@@ -2,7 +2,7 @@ export const newPost = {
 	title: "Ferrari confirm Mattia Binotto has resigned as Team Principal",
 	content:
 		"Binotto, who originally joined the team as an engine engineer back in 1995, took on his current role - encompassing the Gestione Sportiva department - ahead of the 2019 campaign.",
-	categoryIds: [1],
+	categoryId: [1],
 };
 
 export const newPostWithInvalidId = {
@@ -10,11 +10,11 @@ export const newPostWithInvalidId = {
 		"Bitcoin's Price Action Is Encouraging, Yet It May Not Signal Bottom: Traders",
 	content:
 		"It's winter out there in the real word and the digital assets market, yet crypto twitter is going bottom fishing.",
-	categoryIds: [300],
+	categoryId: [300],
 };
 
 export const invalidNewPost = {
 	title: "",
 	content: "",
-	categoryIds: [],
+	categoryId: [],
 };
