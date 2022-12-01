210cb7929011567f37de9ec2ab829460cebc1c80
Maikson <estevan.maik@gmail.com>
2022-12-01 09:22:08 -0300

    chore: cria mocks para testes

---
+++ src/__tests__/mocks/postMockData.ts
@@ -0,0 +1,20 @@
+export const newPost = {
+	title: "Ferrari confirm Mattia Binotto has resigned as Team Principal",
+	content:
+		"Binotto, who originally joined the team as an engine engineer back in 1995, took on his current role - encompassing the Gestione Sportiva department - ahead of the 2019 campaign.",
+	categoryIds: [1],
+};
+
+export const newPostWithInvalidId = {
+	title:
+		"Bitcoin's Price Action Is Encouraging, Yet It May Not Signal Bottom: Traders",
+	content:
+		"It's winter out there in the real word and the digital assets market, yet crypto twitter is going bottom fishing.",
+	categoryIds: [300],
+};
+
+export const invalidNewPost = {
+	title: "",
+	content: "",
+	categoryIds: [],
+};
