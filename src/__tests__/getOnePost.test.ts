a5bda074c1e2a8a12422b197c680d6d584f8c073
Maikson <estevan.maik@gmail.com>
2022-12-04 10:31:04 -0300

    refactor: corrige rota nos testes

--- src/__tests__/getOnePost.test.ts
+++ src/__tests__/getOnePost.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../app";
 import { StatusCodes } from "http-status-codes";
 
-describe("GET /post", () => {
+describe("GET /post/:id", () => {
 	describe("Users with valid token", () => {});
 
 	describe("With invalid token", () => {
@@ -11,7 +11,7 @@ describe("GET /post", () => {
 		it("should respond with status code 401", async () => {
 			for (const token of invalidTokenCases) {
 				const response = await request(app)
-					.get("/post")
+					.get("/post/1")
 					.set("Authorization", token);
 
 				expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
@@ -20,7 +20,7 @@ describe("GET /post", () => {
 
 		it("should respond with correct message in case of empty token", async () => {
 			const response = await request(app)
-				.get("/post")
+				.get("/post/1")
 				.set("Authorization", invalidTokenCases[1]);
 
 			expect(response.body.message).toBe("Token not found");
@@ -28,7 +28,7 @@ describe("GET /post", () => {
 
 		it("should respond with correct message in case of invalid token", async () => {
 			const response = await request(app)
-				.get("/post")
+				.get("/post/1")
 				.set("Authorization", invalidTokenCases[0]);
 
 			expect(response.body.message).toBe("Expired or invalid token");
