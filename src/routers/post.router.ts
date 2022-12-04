301254fb47c3811b16674024f13f8699eab5db7a
Maikson <estevan.maik@gmail.com>
2022-12-04 10:39:01 -0300

    chore: adiciona função à rota

--- src/routers/post.router.ts
+++ src/routers/post.router.ts
@@ -2,13 +2,17 @@ import express from "express";
 import { PostSchema } from "../middlewares/joiSchemas/post.schema";
 import { validateSchema } from "../middlewares/validateSchemas";
 import { authenticationMiddleware } from "../middlewares/auth.middleware";
-import { createNewPost, getAllPosts } from "../controllers/post.controller";
+import {
+	createNewPost,
+	getAllPosts,
+	getPostById,
+} from "../controllers/post.controller";
 require("express-async-errors");
 
 const routers = express.Router();
 
 routers.get("/", authenticationMiddleware, getAllPosts);
-routers.get("/:id", authenticationMiddleware);
+routers.get("/:id", authenticationMiddleware, getPostById);
 routers.post(
 	"/",
 	authenticationMiddleware,
