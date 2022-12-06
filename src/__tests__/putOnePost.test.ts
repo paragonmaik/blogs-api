ba0c5bda272d58d50097142c9b6dc8a3a9edcf67
Maikson <estevan.maik@gmail.com>
2022-12-06 17:12:42 -0300

    chore: testes PUT /post/:id

--- src/__tests__/putOnePost.test.ts
+++ src/__tests__/putOnePost.test.ts
@@ -1,10 +1,98 @@
 import request from "supertest";
 import app from "../app";
 import { StatusCodes } from "http-status-codes";
-import { loginData } from "./mocks/loginMockData";
+import { lewisLogindata, michaelLoginData } from "./mocks/loginMockData";
+import { invalidEditPostData, editPostData } from "./mocks/postMockData";
 
 describe("PUT /post/:id", () => {
-	describe("Users with valid token", () => {});
+	describe("Users with valid token", () => {
+		it("should respond with status code 200", async () => {
+			const loginResponse = await request(app)
+				.post("/login")
+				.send(lewisLogindata);
+
+			const response = await request(app)
+				.put("/post/1")
+				.send(editPostData)
+				.set("Authorization", loginResponse.body.token);
+
+			expect(response.status).toBe(StatusCodes.OK);
+		});
+
+		it("should return one edited post", async () => {
+			const loginResponse = await request(app)
+				.post("/login")
+				.send(lewisLogindata);
+
+			const post = await request(app)
+				.put("/post/1")
+				.send(editPostData)
+				.set("Authorization", loginResponse.body.token);
+
+			expect(post.body.id).toBeDefined();
+			expect(post.body.title).toBeDefined();
+			expect(post.body.content).toBeDefined();
+			expect(post.body.userId).toBeDefined();
+			expect(post.body.published).toBeDefined();
+			expect(post.body.updated).toBeDefined();
+			expect(post.body.user).toBeDefined();
+			expect(post.body.user.password).toBeUndefined();
+		});
+	});
+
+	describe("Request with unauthorized user", () => {
+		it("should respond with status code 401", async () => {
+			const loginResponse = await request(app)
+				.post("/login")
+				.send(michaelLoginData);
+
+			const response = await request(app)
+				.put("/post/1")
+				.send(editPostData)
+				.set("Authorization", loginResponse.body.token);
+
+			expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+		});
+
+		it("should respond with correct error message in case of unauthorized user", async () => {
+			const loginResponse = await request(app)
+				.post("/login")
+				.send(michaelLoginData);
+
+			const response = await request(app)
+				.put("/post/1")
+				.send(editPostData)
+				.set("Authorization", loginResponse.body.token);
+
+			expect(response.body.message).toBe("Unauthorized user");
+		});
+	});
+
+	describe("Request with incorrect data", () => {
+		it("should respond with status code 400", async () => {
+			const loginResponse = await request(app)
+				.post("/login")
+				.send(lewisLogindata);
+
+			const response = await request(app)
+				.put("/post/1")
+				.set("Authorization", loginResponse.body.token);
+
+			expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+		});
+
+		it("should respond with correct error message in case of missing fields", async () => {
+			const loginResponse = await request(app)
+				.post("/login")
+				.send(lewisLogindata);
+			const response = await request(app)
+				.put("/post/1")
+				.send(invalidEditPostData)
+				.set("Authorization", loginResponse.body.token);
+
+			expect(response.body.message).toBe("Some required fields are missing");
+		});
+	});
 
 	describe("With invalid token", () => {
 		const invalidTokenCases = ["invalid-token", ""];
